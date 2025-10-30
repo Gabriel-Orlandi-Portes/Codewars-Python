4f2279b4907518286a9f3927be49059c8e6b3f84
Gabriel Orlandi Portes <163611471+Gabriel-Orlandi-Portes@users.noreply.github.com>
2025-10-30 11:58:25 -0300

    Create Clock.py

---
+++ src/8Kyu/Clock.py
@@ -0,0 +1,8 @@
+# Objetivo: Conversão para milissegundos
+def past(h, m, s): 
+    
+    return (h * 3600 + m * 60 + s) * 1000
+
+# 1 hora = 3600 segundos
+# 1 minuto = 60 segundos
+# 1 segundo = 1000 Milissegundos
