328e6035e37c48c783101a4407239b81d9a6e236
Gabriel <gabrielorlandi220@gmail.com>
2026-07-07 23:50:00 -0300

    feat: add solution for What'sTheRealFloor.py

--- src/8Kyu/What'sTheRealFloor.py
+++ src/8Kyu/What'sTheRealFloor.py
@@ -1,8 +1,7 @@
-def calculate_years(principal, interest, tax, desired):
-    years = 0
-
-    while principal < desired:
-        principal += principal * interest * (1 - tax)
-        years += 1
-
-    return years
+def get_real_floor(n):
+    if n <= 0:
+        return n
+    elif n < 13:
+        return n - 1
+    else:
+        return n - 2
